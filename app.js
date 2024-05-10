49e5f0c2283d03ce913e5fab00d7e3e7de63def9
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-09 21:38:30 -0400

    fetch data

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('products starter');
+const url = "https://www.course-api.com/javascript-store-products";
+
+const productsDOM = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchProducts();
