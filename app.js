d8ceec4bfc42e816fbdfca102d4a3bbd44541cff
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-10 05:51:44 -0400

    single product html page

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ const url = "https://www.course-api.com/javascript-store-products";
 const productsDOM = document.querySelector(".products-center");
 
 const fetchProducts = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
   try {
     const resp = await fetch(url);
     const data = await resp.json();
-    console.log(data);
+    return data;
   } catch (err) {
-    console.log(err);
+    productsDOM.innerHTML = `<p class="error">there was an error</p>`;
   }
 };
 
-fetchProducts();
+const displayProducts = (list) => {
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price } = product.fields;
+      const { url: img } = product.fields.image[0];
+      const formatPrice = price / 100;
+      // id, name, price, img
+      return `<a href="product.html?id=${id}" class="single-product">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">$${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join("");
+  productsDOM.innerHTML = `<div class="products-container">${productList}</div>`;
+};
+
+const start = async () => {
+  const data = await fetchProducts();
+  displayProducts(data);
+};
+
+start();
