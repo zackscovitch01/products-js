88bef56aa44250eefb793c8ed9a7f5e5705555d2
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-10 06:13:34 -0400

    fetch unique product using ids

---
+++ product.js
@@ -0,0 +1,29 @@
+const url = "https://www.course-api.com/javascript-store-single-product";
+
+const productDOM = document.querySelector(".product");
+
+const fetchProduct = async () => {
+  try {
+    productDOM.innerHTML = '<h4 class="product-loading">Loading...</h4>';
+
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML = `<p class="error">There was a problem loading the product. Please try again later </p>`;
+  }
+};
+
+const displayProduct = (product) => {
+  console.log(product);
+};
+
+const start = async () => {
+  const data = await fetchProduct();
+  displayProduct(data);
+};
+
+start();
